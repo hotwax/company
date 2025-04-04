595667318ac91d27938a0c8836abb75f67880c0d
R-Sourabh <rsourabh002@gmail.com>
2025-04-04 18:17:40 +0530

    Improved: added configs for pwa,  robots.txt file and updated the manifest.json file (dxp/379)

--- vue.config.js
+++ vue.config.js
@@ -13,6 +13,17 @@ module.exports = {
       enableInSFC: true
     }
   },
+  pwa: {
+    name: "Company - HotWax Commerce",
+    themeColor: "#FFFFFF",
+    manifestOptions: {
+      short_name: "Company",
+      start_url: "./"
+    },
+    id: "/",
+    display: "standalone",
+    background_color: "#000000"
+  },
   configureWebpack: {
     resolve: {
       alias: {
